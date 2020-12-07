050a5d3c55013fb7839023753c99ddbce5bbed0b
caamxe <64737283+caamxe@users.noreply.github.com>
2020-12-07 12:49:06 +0100

    added compression code

---
+++ source/compression.h
@@ -0,0 +1,8 @@
+#ifndef HUFFMAN_PROJECT_COMPRESSION_H
+#define HUFFMAN_PROJECT_COMPRESSION_H
+
+#include "../HT/HuffmanTree.h"
+
+void b_compression(char *target);
+
+#endif //HUFFMAN_PROJECT_COMPRESSION_H
