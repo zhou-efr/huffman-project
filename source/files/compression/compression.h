68a2234788b4692c95cc38bd66f8fff710032cae
zhou <killian.zhou@sepefrei.fr>
2020-12-11 20:35:46 +0100

    compression.h commentary

--- source/files/compression/compression.h
+++ source/files/compression/compression.h
@@ -6,15 +6,16 @@
 /**
  * This function compresses a given file
  * @param target is the file we want to compress
+ * @return whether the function success or not
  */
 int compression(char *target);
 
 /**
  *
- * @param target
- * @param arbre
- * @param uncompressChar
- * @return
+ * @param target : path to the compressed file
+ * @param arbre : path to the saved tree
+ * @param uncompressChar : path to the file where the output will be write
+ * @return whether the function success or not
  */
 int decompression(char* target, char *arbre, char *uncompressChar);
 
